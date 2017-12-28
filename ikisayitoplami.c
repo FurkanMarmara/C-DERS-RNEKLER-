b60442fa9b3326a6f52cfa4562011540916301ed
FurkanMarmara <33186256+FurkanMarmara@users.noreply.github.com>
2017-12-28 11:18:08 +0200

    Create ikisayitoplami.c

---
+++ ikisayitoplami.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+main()
+{
+int sayi1;
+int sayi2;
+int toplam;
+
+printf("1.Sayiyi Giriniz : ");
+scanf("%d",&sayi1);
+printf("2.Sayiyi Giriniz : ");
+scanf("%d",&sayi2);
+toplam=sayi1+sayi2;
+printf("Sayilarin Toplami : %d",toplam);
+getch();
+
+}
