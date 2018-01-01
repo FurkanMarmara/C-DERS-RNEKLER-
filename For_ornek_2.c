9ac79dcdc286be1c8e76a3c7ce0d6a066e66fc91
FurkanMarmara <33186256+FurkanMarmara@users.noreply.github.com>
2018-01-01 21:01:00 +0200

    Create For_ornek_2.c

---
+++ For_ornek_2.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+#include <conio.h>
+main()
+{
+//1den 100 e kadar sayıları yazdıran program.
+int sayi=1;
+  for(int i=0;i<100;i++)
+  {
+    printf("%d",sayi);
+    sayi++;
+  }
+  getch();
+}
