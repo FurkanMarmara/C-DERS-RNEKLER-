57d32182772264b5f098b86de180adfaa2a2b451
FurkanMarmara <33186256+FurkanMarmara@users.noreply.github.com>
2018-01-03 22:46:46 +0200

    Create if_anlatım.c

---
+++ if_anlatım.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+main()
+{
+int girilensayi;
+printf ("Sayı Giriniz : ");
+scanf("%d",&girilensayi);
+  if(girilensayi==5) // Burdaki if kelimesi türkçede eğer anlamına gelmektedir Eğer kullanıcının girdiği sayı 5 ise aşağısındaki kodlar 
+  //çalışacak
+  {
+    Printf("Merhaba Dünya");
+  }
+  getch();
+}
